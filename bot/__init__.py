ddba14f1756ac1ef559a6d92b55497f60965f2a7
tarunprasad45 <tarunprasad1845@gmail.com>
2026-04-22 17:25:45 +0530

    chore: initialize bot package with public API exports

---
+++ bot/__init__.py
@@ -0,0 +1,7 @@
+"""
+trading-bot.bot - core execution package
+
+Public API:
+     from bot.orders import place_market_order, place_limit_order
+     from bot.execptions import BinanceAPIError, ValidationError
+"""
